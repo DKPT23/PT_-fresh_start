5c7550b107ee47eb7dae2aef85246294e8fa5d1d
DKPT23 <120628848+DKPT23@users.noreply.github.com>
2022-12-15 09:45:44 +0200

    V1 install choco.exe and install programs
    we created a new PT script that will give a new penetration tester a fresh start with all the tools he need

---
+++ script.ps1
@@ -0,0 +1,8 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install -y genymotion --ignore-checksums virtualbox slack burp-suite-pro-edition forticlientvpn Notepadplusplus 7zip git adb go Firefox openssl microsoft-windows-terminal docker-desktop
+wsl --install
+wsl --install -d kali-linux
+kali config --default-user root
+wsl apt update
+wsl apt install -y kali-linux-headless
+ 
