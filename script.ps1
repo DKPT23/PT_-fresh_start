72cb8b8bb056bfec2394cc423e93a75416e71cf7
DKPT23 <120628848+DKPT23@users.noreply.github.com>
2022-12-15 10:05:03 +0200

    greenshot
    add greenshot to the script

--- script.ps1
+++ script.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install -y genymotion --ignore-checksums virtualbox slack burp-suite-pro-edition forticlientvpn Notepadplusplus 7zip git adb go Firefox openssl microsoft-windows-terminal docker-desktop
+choco install -y genymotion --ignore-checksums virtualbox slack burp-suite-pro-edition forticlientvpn Notepadplusplus 7zip git adb go Firefox openssl microsoft-windows-terminal docker-desktop greenshot
 wsl --install
 wsl --install -d kali-linux
 kali config --default-user root
