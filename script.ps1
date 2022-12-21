f7c2c3c6a3b15cc370f9ff60fd417dd97e46cfff
root <root@Yechiel.localdomain>
2022-12-21 09:41:31 +0200

    split to 2 files

--- script.ps1
+++ script.ps1
@@ -1,40 +1,18 @@
-function before{
-    Set-ExecutionPolicy Bypass
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    
-    #use choco to install packages
-    choco install -y virtualbox slack burp-suite-pro-edition forticlientvpn Notepadplusplus 7zip git adb go Firefox openssl microsoft-windows-terminal docker-desktop greenshot nmap GoogleChrome wireshark python javaruntime android-sdk powertoys
-    choco install -y genymotion --ignore-checksums
-    wsl --install
-    
-    
-    $loginScriptPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\wsl-install-kali.bat"
-    
-    Add-Content -Path $loginScriptPath -Value 'powershell _____ -File -Command after'
-    Add-Content -Path $loginScriptPath -Value 'DEL "%~f0"'
-    
-    Restart-Computer 
-}
+Set-ExecutionPolicy Bypass
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+#use choco to install packages
+choco install -y virtualbox slack burp-suite-pro-edition forticlientvpn Notepadplusplus 7zip git adb go Firefox openssl microsoft-windows-terminal docker-desktop greenshot nmap GoogleChrome wireshark python javaruntime android-sdk powertoys
+choco install -y genymotion --ignore-checksums
+wsl --install
 
 
-function after{
-    #create logon script that will install kali 
-    wsl --set-default-version 2
-    wsl --install -d kali-linux
-    wsl --setdefault Kali-linux 
-    kali config --default-user root
-    wsl apt update
-    wsl apt install -y kali-linux-headless seclists nuclei
-    
-    Set-Location C:Android\Sdk\tools\bin
-    
-    ./sdkmanager.bat "platform-tools" "platforms;android-28"
-    ./sdkmanager.bat "system-images;android-28;google_apis;x86_64"
-    for($i=0;$i -lt 30;$i++) { $response += "y`n"}; $response | sdkmanager --licenses
-    
-    Write-Output "no" | avdmanager create avd -n Android28 -k "system-images;android-28;google_apis;x86_64"
-    
-   write-output "./../emulator.exe -writable-system -no-snapshot -avd Android28"
+$loginScriptPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\wsl-install-kali.bat"
 
-}
+Add-Content -Path $loginScriptPath -Value 'powershell  iex ((New-Object System.Net.WebClient).DownloadString("https://github.com/DKPT23/PT_fresh_start/raw/2file/script.ps1"));'
+Add-Content -Path $loginScriptPath -Value 'DEL "%~f0"'
+
+Restart-Computer 
+
+
 
---
+++ script2.ps1
@@ -0,0 +1,18 @@
+wsl --set-default-version 2
+wsl --install -d kali-linux
+wsl --setdefault Kali-linux 
+kali config --default-user root
+wsl apt update
+wsl apt install -y kali-linux-headless seclists nuclei
+
+Set-Location C:Android\Sdk\tools\bin
+
+./sdkmanager.bat "platform-tools" "platforms;android-28"
+./sdkmanager.bat "system-images;android-28;google_apis;x86_64"
+for($i=0;$i -lt 30;$i++) { $response += "y`n"}; $response | sdkmanager --licenses
+
+Write-Output "no" | avdmanager create avd -n Android28 -k "system-images;android-28;google_apis;x86_64"
+
+write-output "./../emulator.exe -writable-system -no-snapshot -avd Android28"
+
+
