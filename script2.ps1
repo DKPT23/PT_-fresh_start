9807cc352dc58933da45f6036adf5dc02def6db6
Yechiel <yechielworen@gmail.com>
2023-01-09 11:01:51 +0200

    fixed path

--- script2.ps1
+++ script2.ps1
@@ -6,13 +6,13 @@ wsl apt update
 wsl apt install -y kali-linux-headless seclists nuclei golang-go tmux neovim
 wsl chsh /bin/zsh
 
-Set-Location C:Android\Sdk\tools\bin
 
-./sdkmanager.bat "platform-tools" "platforms;android-28"
-./sdkmanager.bat "system-images;android-28;google_apis;x86_64"
-for($i=0;$i -lt 30;$i++) { $response += "y`n"}; $response | sdkmanager --licenses
 
-Write-Output "no" | avdmanager create avd -n Android28 -k "system-images;android-28;google_apis;x86_64"
+C:\Android\android-sdk\tools\bin\sdkmanager.bat "platform-tools" "platforms;android-28"
+C:\Android\android-sdk\tools\bin\sdkmanager.bat "system-images;android-28;google_apis;x86_64"
+for($i=0;$i -lt 30;$i++) { $response += "y`n"}; $response | C:\Android\android-sdk\tools\bin\sdkmanager.bat --licenses
+
+Write-Output "no" | C:\Android\android-sdk\tools\bin\avdmanager.bat create avd -n Android28 -k "system-images;android-28;google_apis;x86_64"
 
 write-output "./../emulator.exe -writable-system -no-snapshot -avd Android28"
 
