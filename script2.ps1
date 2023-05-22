cddba16eaf52b3591d5271bbd6842abc3bfe7e13
Yechiel <41305372+yechielw@users.noreply.github.com>
2023-05-22 15:56:14 +0300

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -1,19 +1,12 @@
-cmd wsl --set-default-version 2
-cmd wsl --install -d kali-linux
-cmd wsl --setdefault Kali-linux 
-cmd kali config --default-user root
-cmd wsl apt update
-cmd wsl apt install -y kali-linux-headless seclists nuclei golang-go tmux neovim
-cmd wsl chsh /bin/zsh
+winget install PortSwigger.BurpSuite.Professional 9PKR34TNCV07 --accept-package-agreements --accept-source-agreements
 
+choco install -y --ignore-checksums gsudo genymotion virtualbox slack forticlientvpn Notepadplusplus 7zip git adb go Firefox openssl microsoft-windows-terminal docker-desktop greenshot nmap GoogleChrome wireshark python javaruntime powertoys
 
+wsl --setdefault Kali-linux
+kali config --default-user root
+wsl apt update
+wsl apt install -y kali-linux-headless seclists nuclei golang-go
+wsl chsh -s /bin/zsh
 
-C:\Android\android-sdk\tools\bin\sdkmanager.bat "platform-tools" "platforms;android-28"
-C:\Android\android-sdk\tools\bin\sdkmanager.bat "system-images;android-28;google_apis;x86_64"
-for($i=0;$i -lt 30;$i++) { $response += "y`n"}; $response | C:\Android\android-sdk\tools\bin\sdkmanager.bat --licenses
-
-Write-Output "no" | C:\Android\android-sdk\tools\bin\avdmanager.bat create avd -n Android28 -k "system-images;android-28;google_apis;x86_64"
-
-write-output "./../emulator.exe -writable-system -no-snapshot -avd Android28"
 
 
