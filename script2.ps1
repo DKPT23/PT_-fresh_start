3e85c3bfd7539ed479c6963c150878b3561a5042
Yechiel <41305372+yechielw@users.noreply.github.com>
2023-05-22 16:57:38 +0300

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -7,7 +7,7 @@ winget install PortSwigger.BurpSuite.Professional 9PKR34TNCV07 --accept-package-
 
 choco install -y --ignore-checksums gsudo genymotion virtualbox slack forticlientvpn Notepadplusplus 7zip git adb go Firefox openssl microsoft-windows-terminal docker-desktop greenshot nmap GoogleChrome wireshark python javaruntime powertoys
 
-Install-Module PSReadLine -allowPrerelease
+Install-Module PSReadLine -Force
 Install-Module PSFzf
 iwr https://github.com/yechielw/windows-deploy/raw/main/profile.ps1 -outfile $PROFILE
 
