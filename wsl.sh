e0de13ce67c7993019e19a0c2d83cd527c6eb6b2
Yechiel <41305372+yechielw@users.noreply.github.com>
2023-11-01 13:57:38 +0200

    Update wsl.sh
    fixed cme url

--- wsl.sh
+++ wsl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update && apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y kali-linux-headless seclists nuclei golang-go neovim pipx python3-venv
-pipx install git+https://github.com/Porchetta-Industries/CrackMapExec
+pipx install git+https://github.com/byt3bl33d3r/CrackMapExec
 chsh -s /usr/bin/zsh
 sed -i 's/#\[ "\$EUID"/\[ "\$EUID"/' $HOME/.zshrc
 echo 'export PATH="$PATH:/opt:$HOME/.local/bin:$HOME/go/bin"' >> $HOME/.zshrc 
