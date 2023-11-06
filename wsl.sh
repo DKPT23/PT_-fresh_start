db79c4b8666b14f1dac922ba93ea64ef059ad4b1
Yechiel <41305372+yechielw@users.noreply.github.com>
2023-11-06 09:22:31 +0200

    fixed path

--- wsl.sh
+++ wsl.sh
@@ -4,4 +4,4 @@ apt update && apt -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--for
 pipx install git+https://github.com/byt3bl33d3r/CrackMapExec
 chsh -s /usr/bin/zsh
 sed -i 's/#\[ "\$EUID"/\[ "\$EUID"/' $HOME/.zshrc
-echo 'export PATH="$PATH:/opt:$HOME/.local/bin:$HOME/go/bin"' >> $HOME/.zshrc 
+echo 'export PATH="/opt:$HOME/.local/bin:$HOME/go/bin:$PATH"' >> $HOME/.zshrc 
